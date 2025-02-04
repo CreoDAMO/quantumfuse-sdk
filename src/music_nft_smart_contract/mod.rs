f3de10848892320a644a25e1114c4f83966ab096
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:50:32 -0500

    Update mod.rs

--- src/music_nft_smart_contract/mod.rs
+++ src/music_nft_smart_contract/mod.rs
@@ -1,4 +1,4 @@
-pub fn deploy_music_nft_contract() -> Result<(), Box> {
+pub fn deploy_music_nft_contract() -> Result<(), Box<dyn std::error::Error>> {
 println!("🎼 Deploying Music NFT Smart Contract... ✅");
 Ok(())
 }
