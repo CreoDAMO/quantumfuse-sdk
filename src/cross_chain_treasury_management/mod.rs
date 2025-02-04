e8a08cc259600cc3cab621e2c02891ea50091dba
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:46:57 -0500

    Update mod.rs

--- src/cross_chain_treasury_management/mod.rs
+++ src/cross_chain_treasury_management/mod.rs
@@ -1,4 +1,4 @@
-pub fn manage_cross_chain_treasury() -> Result<(), Box> {
+pub fn manage_cross_chain_treasury() -> Result<(), Box<dyn std::error::Error>> {
 println!("🔄 Managing Cross-Chain Treasury... ✅");
 Ok(())
 }
