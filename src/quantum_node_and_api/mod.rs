a98df4172b155d6bb37b0b09b1ed06d509b2f66a
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:55:36 -0500

    Update mod.rs

--- src/quantum_node_and_api/mod.rs
+++ src/quantum_node_and_api/mod.rs
@@ -1,4 +1,4 @@
-pub fn setup_quantum_node() -> Result<(), Box> {
+pub fn setup_quantum_node() -> Result<(), Box<dyn std::error::Error>> {
 println!("🔗 Setting up Quantum Node & API... ✅");
 Ok(())
 }
