e4de6f882159173183f0fe9ba01b2cae3b3111cb
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 17:57:11 -0500

    Update mod.rs

--- src/state_manager/mod.rs
+++ src/state_manager/mod.rs
@@ -1,4 +1,4 @@
-pub fn handle_state_management() -> Result<(), Box> {
+pub fn handle_state_management() -> Result<(), Box<dyn std::error::Error>> {
 println!("⚙️ Managing Blockchain State... ✅");
 Ok(())
 }
