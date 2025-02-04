8fb17a9729e9a051b374e7668ee412ce086961da
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:54:27 -0500

    Update mod.rs

--- src/quantum_medical_management/mod.rs
+++ src/quantum_medical_management/mod.rs
@@ -1,4 +1,4 @@
-pub fn manage_medical_data() -> Result<(), Box> {
+pub fn manage_medical_data() -> Result<(), Box<dyn std::error::Error>> {
 println!("🏥 Managing Quantum-Enhanced Medical Data... ✅");
 Ok(())
 }
