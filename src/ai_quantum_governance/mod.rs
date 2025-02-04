5ffdb7817e5e6bc5a1962a30c4f75caf6aec7953
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 17:56:30 -0500

    Update mod.rs

--- src/ai_quantum_governance/mod.rs
+++ src/ai_quantum_governance/mod.rs
@@ -1,4 +1,4 @@
-pub fn manage_quantum_governance() -> Result<(), Box> {
+pub fn manage_quantum_governance() -> Result<(), Box<dyn std::error::Error>> {
 println!("🏛 Managing Quantum Governance... ✅");
 Ok(())
 }
