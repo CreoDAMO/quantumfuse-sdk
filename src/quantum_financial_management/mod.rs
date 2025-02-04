105bf4e7d26a2922191f42e3975900e93e482788
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:53:49 -0500

    Update mod.rs

--- src/quantum_financial_management/mod.rs
+++ src/quantum_financial_management/mod.rs
@@ -1,4 +1,4 @@
-pub fn manage_quantum_finance() -> Result<(), Box> {
+pub fn manage_quantum_finance() -> Result<(), Box<dyn std::error::Error>> {
 println!("💰 Managing Quantum Financial Operations... ✅");
 Ok(())
 }
