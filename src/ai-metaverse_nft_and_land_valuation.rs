ca47cd392160209b428fba3b2757aca10176cd97
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 02:54:46 -0500

    Delete src directory

--- src/ai-metaverse_nft_and_land_valuation.rs
+++
@@ -1,6 +0,0 @@
-use quantumfuse_sdk::{metaverse::NFTMarketplace, ai::PolicyAI};
-
-fn evaluate_nft_market(nft_market: &NFTMarketplace, ai_engine: &PolicyAI) {
-    let price_prediction = ai_engine.predict_nft_value(nft_market.get_recent_sales());
-    println!("🎨 NFT Price Prediction: {:?}", price_prediction);
-}
