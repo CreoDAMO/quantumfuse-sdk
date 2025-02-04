3926062e7007de133c8bd8daf766a096829249cc
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-04 14:57:39 -0500

    Update mod.rs

--- src/quantum_services/mod.rs
+++ src/quantum_services/mod.rs
@@ -1,4 +1,4 @@
-pub fn manage_quantum_services() -> Result<(), Box> {
+pub fn manage_quantum_services() -> Result<(), Box<dyn std::error::Error>> {
 println!("⚡ Managing Quantum Services... ✅");
 Ok(())
 }
