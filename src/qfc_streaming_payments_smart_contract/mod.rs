db1a1e8ebf5cd28a5a51febd9a1e201890e70ad9
Jacque Antoine DeGraff <jacquedegraff_creo@outlook.com>
2025-02-05 16:59:36 -0500

    Update mod.rs

--- src/qfc_streaming_payments_smart_contract/mod.rs
+++ src/qfc_streaming_payments_smart_contract/mod.rs
@@ -1,5 +1,3 @@
-pub mod qfc_streaming_payments_smart_contract;
-
 pub fn handle_streaming_payments() -> Result<(), Box<dyn std::error::Error>> {
     println!("🎼 Handling Streaming Payments... ✅");
     Ok(())
